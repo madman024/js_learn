95e5b4332e1522bbe566319ff7e00b55c4ee24fe
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-01 16:38:22 +0000

    day one

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const pie=3.1415
+console.log(pie);
+
+pie=4563762
+console.log(pie);
+
+/* 1)const keyword is used for declaring a const 
+    constant cannot be modified
+   2)var keyword was used earlier but it has scope problem so we use let 
+   3)we can also not declare the type but not recommende  
+
+*/
