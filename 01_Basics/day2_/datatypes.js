5704caf1d4cea40dc5b9ff17f440beff0f07c817
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-02 17:46:46 +0000

    data types

---
+++ 01_Basics/day2_/datatypes.js
@@ -0,0 +1,17 @@
+/*
+
+"use strict"; code is treated as newer version of js
+alert() works different in node than browser
+
+
+1)number
+2)bigint
+3)boolean
+4)string
+4)null
+5)undefined
+
+null is an object
+"typeof" keyword is used to check data type
+
+*/
