1593589c23179025930576b187ff82ed6c0160cd
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-02 18:14:23 +0000

    learning from mistakes

--- 01_Basics/day2_/data_conversion.js
+++ 01_Basics/day2_/data_conversion.js
@@ -18,3 +18,15 @@ true-1  false-0
 ""--false
 "hoh"--true
 */
+
+
+let pie1='543'
+console.log(pie);
+console.log(typeof pie);
+let pie1v=Number(pie)
+console.log(piev);
+console.log(typeof piev);
+
+/*
+Number keyword for convesion to number
+*/
--- 01_Basics/day2_/datatypes.js
+++ 01_Basics/day2_/datatypes.js
@@ -20,3 +20,28 @@ null is an object
 "typeof" keyword is used to check data type
 
 */
+
+let something1 ='nothing'
+console.log(something)
+console.log(typeof something);
+
+
+
+
+/*
+"use strict"; treat all code as newer version of JS
+alet() worlks different in node 
+
+1)number
+2)bigint
+3)string
+4)boolean
+5)null
+6)undefined
+
+null is an object
+
+
+
+
+*/
