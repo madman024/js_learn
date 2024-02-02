7a12fbbfc2ef8cb980dc016be16e91d1347f27cd
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-02 18:00:52 +0000

    conversion

---
+++ 01_Basics/day2_/data_conversion.js
@@ -0,0 +1,20 @@
+let pie='3553'
+console.log(pie);
+console.log(typeof pie);
+
+let piev=Number(pie)
+console.log(piev);
+console.log(typeof piev);
+
+/*
+Number keyword is used for converting to number 
+
+"57"--57
+"64abd"--Nan
+true-1  false-0
+
+
+
+""--false
+"hoh"--true
+*/
