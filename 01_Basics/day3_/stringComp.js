2e2bf8b9612110d101a2f3ca52093c75581d22b6
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-03 15:17:44 +0000

    checker

---
+++ 01_Basics/day3_/stringComp.js
@@ -0,0 +1,13 @@
+/*
+
+comparing string and numbers ,js convetrts numbers to string
+
+comparision and equal to operator works deferntly 
+
+mostly dont compare diff data types
+
+=== strict check ,it checks the data type and value
+
+kya chalra hai yaar 1020
+
+*/
