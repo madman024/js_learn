f2d9ab2c8fe0a880cb69962338d282d987a0b9de
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-03 15:06:34 +0000

    string con

---
+++ 01_Basics/day3_/stringConversion.js
@@ -0,0 +1,11 @@
+/*
+
+there are various rules to be followed while string conversion 
+ig whats the first operation that decides the type 
+
+prefix and postfix 
+prefix :returns the value and increments the base variable
+postfix:increments the base variable and returns the value
+
+ecma mdn full web 
+*/
