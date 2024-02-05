0161a7cf33010f7250153b47496f30658d09dcd3
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-05 14:38:01 +0000

    cat mem

---
+++ 01_Basics/day5_/memory.js
@@ -0,0 +1,31 @@
+console.log("hello memory")
+
+let myname='hrishikesh'
+new_name=myname
+new_name='madman'
+console.log(myname);
+console.log(new_name);
+
+let user1={
+ name:"samson",
+ id:24
+
+}
+let user2=user1
+user2.id=21
+
+console.log(user1)
+console.log(user2)
+/*
+
+stack memory--prmitive--pass by value
+a copy of value is chared 
+
+heap memory--non primitive--pass by reference
+if we equate to variables then they both point to a single mrmory location in heaop
+
+
+
+*/
+
+// cats are gone ,
