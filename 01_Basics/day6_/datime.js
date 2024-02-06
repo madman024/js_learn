5d7385ab681e9e4592f78e7e8017e5a704fd59eb
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-06 15:04:29 +0000

    01 complete hash

---
+++ 01_Basics/day6_/datime.js
@@ -0,0 +1,8 @@
+let mydate=new Date()
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toString());
+console.log(mydate.toJSON());
+
+// i will create a project based on this that will show exact time from ypor birth,celebrate milestones,interact along a virtual reality and more
