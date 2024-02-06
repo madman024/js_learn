c123e43dd611d5da6aefde963048e31a94f7207a
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-06 14:24:00 +0000

    math and num

---
+++ 01_Basics/day6_/num.js
@@ -0,0 +1,25 @@
+const score=53176
+console.log(score);
+
+const balaance=new Number(354)
+console.log(balaance);
+
+// .toString() convert to string ,we can apply string methods
+// .length
+
+// .toFixed(n) gives precision of number upto n digits after decimal point 
+
+
+// .toLocalString('en-IN)
+
+// ============================math
+/*
+
+Math.abs()
+Math.round()
+Math.seil()
+Math.floor()
+Math.random()
+*/
+
+console.log(Math.floor(Math.random()*10))
