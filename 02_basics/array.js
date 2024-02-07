92e47bf5930522c1bfb2696e85b4c0af38b06bb7
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-07 16:03:21 +0000

    arrey

---
+++ 02_basics/array.js
@@ -0,0 +1,27 @@
+// Arrays
+
+const array=[20,21,15,24]
+
+console.log(array);
+console.log(array[3]);
+
+const obarray=new Array(1,2,3,4)
+obarray.push(32)
+obarray.push(61)
+obarray.pop()
+console.log(obarray);
+
+/*
+
+.includes(n)
+.indexOf(n)
+
+myarray.join()--convert array to a string
+
+.slice(a,b)--start index a(included)
+             end index b (not included )
+
+.splice(a,b)--both indexes included and it removes those elements from the main array
+
+8.91 --
+*/
