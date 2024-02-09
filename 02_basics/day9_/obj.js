0e0fc97b85b70de9d8d3e47aa13b4659afc67a52
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-09 12:23:29 +0000

    tml holi obj

---
+++ 02_basics/day9_/obj.js
@@ -0,0 +1,20 @@
+const user =new Object()  
+// singleton object 
+const user1={}
+// non singleton object 
+
+console.log(user)
+
+/*
+objects inside object ,use the dot operator to access the components of an object 
+
+
+const store=Object.assign({},obj1,obj2..)
+
+conts store ={...obj1,...obj2}
+
+Object.keys(obj1)
+Object.values(obj2)--stores them in an array
+Object.entries()--create an array of key value
+
+*/
