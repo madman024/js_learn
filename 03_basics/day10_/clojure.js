3e8022adea6c13eb79fb5886e633d42b653a514e
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-10 14:30:30 +0000

    move command

---
+++ 03_basics/day10_/clojure.js
@@ -0,0 +1,8 @@
+//child function can acess parent variables but parent variable cannot acess child variables 
+//cariable--ice cream
+
+//loving this keyboard based text editor 
+
+//clojure is about how to define functions and all 
+
+//ok fine 
