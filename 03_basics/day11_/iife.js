90d9c2717e0e59e708f04524e733d81249da1903
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-11 12:12:47 +0000

    iife

---
+++ 03_basics/day11_/iife.js
@@ -0,0 +1,9 @@
+//
+
+(function say(){
+    console.log("Started")
+})()
+
+//iife is used to immediately excete a function ,to prevent from polluting from global scope
+
+//to have 2 IIFE use ; to stop the 1 one 
