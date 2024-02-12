e8bd3bce956bd9672c4425625fc3a9fb9215b409
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-12 15:59:53 +0000

    ifel

---
+++ 04_controflow/cf.js
@@ -0,0 +1,13 @@
+//if else--control structures are used to run a specific part of the code or in general control the flow of control and part of code execution
+
+let num=21
+
+if (num==24){
+console.log("You are madman")
+}
+else{
+console.log("Who are you")
+}
+
+// === strong check ,it also checks the type of the two operand 
+
