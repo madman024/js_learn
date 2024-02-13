b16beddb9102c7a091c323979b7d5ba009200ffc
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-13 17:21:52 +0000

    LOPS OS PADHNA HAI

---
+++ 04_controflow/day13_/loops.js
@@ -0,0 +1,19 @@
+    // const apie=[13,6,8,34,54,3]
+
+    // console.log(apie)
+
+    // for(let i=0;i<apie.length;i++){
+    //     console.log(apie[i])
+    // }
+
+
+    for (let i = 1; i < 11; i++) {
+        for (let j = 1; j < 11; j++) {
+            console.log(` ${i} * ${j} =${i*j}`)
+            
+        }
+        console.log("New table")
+        
+    }
+
+    // break,continue--pata hai bhai kya hai ,or do I
