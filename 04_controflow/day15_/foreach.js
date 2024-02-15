84cca47a0495d8f8f4f944ec250bdebfdf432e0d
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-15 14:33:58 +0000

    sry loops

---
+++ 04_controflow/day15_/foreach.js
@@ -0,0 +1,38 @@
+const arr=[13,56,7,45,3,657,6]
+
+for (const key in arr) {
+    
+    console.log(key);
+        
+    
+}
+
+arr.forEach(element => {
+   console.log(element) 
+});
+
+
+const greeting="hello samson"
+for (const word of greeting) {
+    console.log(word)
+}
+
+
+// map --is an object which stores in key value pair
+
+const maoo=new Map()
+
+maoo.set('1',"heppio")
+maoo.set("2","javuo")
+
+console.log(maoo)
+
+// skipping a bit sry
+
+/*
+for of loop iterable on array and string
+
+for in loop--iterable on objects
+*/
+
+
