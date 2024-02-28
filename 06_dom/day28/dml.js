ecca17fff6dca714ec80c4913a925edf7edd50e8
madman024 <158486933+madman024@users.noreply.github.com>
2024-02-28 15:08:29 +0000

    just seen

---
+++ 06_dom/day28/dml.js
@@ -0,0 +1,7 @@
+/*
+
+i dont know but doing ,but now i have a little more clarity then a bit of timme before 
+i want to be what i think,not just thinking i want to do it
+*/
+
+// wxacting dom lkef
